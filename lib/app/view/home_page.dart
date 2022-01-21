fda407574354c2c9e1672ebd2b3f312e08ad62ea
Matheus Fernandes <matheusfernandes3m@hotmail.com>
2022-01-21 16:35:48 -0300

    feat: add image and text in home page

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -7,8 +7,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: SearchAppBarWidget(),
-      body: const SizedBox(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 200,
+              child: Image.network(
+                'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+              ),
+            ),
+          ),
+          const Text(
+            'Search any user in github',
+            style: TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
     );
   }
 }
