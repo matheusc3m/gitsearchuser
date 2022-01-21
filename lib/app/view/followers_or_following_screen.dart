2fd597e6ad54ef98d5febf29f7a31e8616c51fe8
Matheus Fernandes <matheusfernandes3m@hotmail.com>
2022-01-21 16:35:28 -0300

    feat: add navigation on custon card widget

--- lib/app/view/followers_or_following_screen.dart
+++ lib/app/view/followers_or_following_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:githubsearch/app/controller/home_page_controller.dart';
 import 'package:githubsearch/app/model/user_model.dart';
 import 'package:githubsearch/app/view/shared/custom_card_widget.dart';
+import 'package:githubsearch/app/view/user_info_screen.dart';
 
 class FollowersOrFollowingScreen extends StatelessWidget {
   String username;
@@ -28,16 +29,27 @@ class FollowersOrFollowingScreen extends StatelessWidget {
           if (snapshot.hasData) {
             return ListView.separated(
               separatorBuilder: (context, index) {
-                return SizedBox(
+                return const SizedBox(
                   height: 20,
                 );
               },
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               itemCount: snapshot.data!.length,
               itemBuilder: (BuildContext context, int index) {
-                return CustomCardWidget(
-                  name: snapshot.data![index]!.username,
-                  avatarUrl: snapshot.data![index]!.avatarUrl,
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => UserInfoScreen(
+                            username: snapshot.data![index]!.username),
+                      ),
+                    );
+                  },
+                  child: CustomCardWidget(
+                    name: snapshot.data![index]!.username,
+                    avatarUrl: snapshot.data![index]!.avatarUrl,
+                  ),
                 );
               },
             );
